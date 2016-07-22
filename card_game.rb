f58f596e054a1cfc16032f91f8ea9d84b6322890
J03L R0J0 <joel.a.rojo@gmail.com>
2016-07-22 10:44:22 -0500

    initial setup

---
+++ card_game.rb
@@ -0,0 +1,14 @@
+class CardGame
+
+  attr_accessor :players
+
+  def initialize(deck, players=["Joel", "Garrett"])
+    @deck = deck
+    @players = players
+  end
+
+  # TODO 
+  def draw_card
+  end
+
+end
